3a2cf3c575a08f94fbed28c98f1df0166eea9eeb
Dimasik1405 <154325028+Dimasik1405@users.noreply.github.com>
2024-02-01 18:15:59 +0300

    Add files via upload

---
+++ Work1.cs
@@ -0,0 +1,28 @@
+﻿
+class sistem 
+{
+    static void Main(string [] arg)
+    {
+        char [,] array = new char[,]
+        {
+            {'a', 'd', 'x'},
+            {'A', 'V', 'P'},
+            {'E', '1', '0'}
+        };
+    string result = CreateRowFromMatrix(array);
+    Console.WriteLine(result);
+    }
+    static string CreateRowFromMatrix(char[,] array)
+    {
+        string str = " ";
+        for(int i = 0; i < array.GetLength(0); i++)
+        {
+            for(int j = 0; j < array.GetLength(1); j++)
+            {
+                str+= array[i, j];
+            }
+        }
+        return str;
+    }
+
+}
